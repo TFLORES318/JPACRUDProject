7b85df2707c2418cfa7c172faabb87e71f1148cb
Tabatha Flores <tabathaflores@SDMacBookPro2.local>
2020-05-15 17:37:37 -0600

    rusticweddingbuiltboot

---
+++ ColoradoRusticWedding/src/main/java/com/skilldistillery/venues/ColoradoRusticWeddingApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.venues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoradoRusticWeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoradoRusticWeddingApplication.class, args);
+	}
+
+}
---
+++ ColoradoRusticWedding/src/test/java/com/skilldistillery/venues/ColoradoRusticWeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.venues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoradoRusticWeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
