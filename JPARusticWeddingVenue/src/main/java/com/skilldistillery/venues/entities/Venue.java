106175d7824457a86916901205c6ef041d51bbdb
Tabatha Flores <tabathaflores@SDMacBookPro2.local>
2020-05-15 19:55:24 -0600

    trying

--- JPARusticWeddingVenue/src/main/java/com/skilldistillery/venues/entities/Venue.java
+++ JPARusticWeddingVenue/src/main/java/com/skilldistillery/venues/entities/Venue.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.venues.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,17 +12,38 @@ public class Venue {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-
 	private String name;
+	private String address;
+	@Column(name="phone_number")
+	private String phoneNumber;
+	private int capacity;
+	@Column(name="full_service")
+	private boolean fullService;
+	private int cost;
+	@Column(name="seasonal_availability")
+	private String seasonalAvailability;
+	@Column(name="handicap_accessible")
+	private boolean handicapAccessible;
+	@Column(name="num_of_bathrooms")
+	private int numOfBathrooms;
 
 	public Venue() {}
 
-	
-	
-	public Venue(int id, String name) {
+
+	public Venue(int id, String name, String address, String phoneNumber, int capacity, boolean fullService, int cost,
+			String seasonalAvailability, boolean handicapAccessible, int numOfBathrooms) {
 		this.id = id;
 		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.capacity = capacity;
+		this.fullService = fullService;
+		this.cost = cost;
+		this.seasonalAvailability = seasonalAvailability;
+		this.handicapAccessible = handicapAccessible;
+		this.numOfBathrooms = numOfBathrooms;
 	}
+
 
 	public int getId() {
 		return id;
